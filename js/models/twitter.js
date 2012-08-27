842b8912686075d97e6c50fe46e8a20a11784f96
Darren Hurst <darren.hurst@broadstreetmobile.com>
2012-08-26 23:36:23 -0400

    cross domain Ajax jsonp fix

--- js/models/twitter.js
+++ js/models/twitter.js
@@ -9,16 +9,24 @@ define([
   		this.getData();
   	},
     url: function () {
-      return 'http://twitter.com/statuses/user_timeline/ITS_A_NERD.json'
+      return 'http://twitter.com/statuses/user_timeline/ITS_A_NERD.json?callback=?'
     },
     getData: function(){
     	var that = this;
-    	$.getJSON(this.url(),function(data){
-    		
-    		that.twitterStream = data;
-    		that.caller.buildTweets(that.twitterStream);
-    	});
+      $.ajax({
+      	url: that.url(),
+    type: 'GET',
+    crossDomain: true,
+    dataType: 'json',
+    success: function(data) {	
+    	console.log(data);
+       	that.twitterStream = data;
+        that.caller.buildTweets(that.twitterStream);},
+    error: function() { alert('Failed!'); }
+    });
+         
     },
+ 
     // Because twitter doesn't return an array of models by default we need
     // to point Backbone.js at the correct property
     parse: function(resp, xhr) {
